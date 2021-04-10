9e211bc3ae443235fe975ab7450fb74c73e5484c
Nguyen Tien <tientt@tientts-MacBook-Pro.local>
2021-04-10 13:51:33 +0700

    init hmodule product

--- Modules/Product/Providers/ProductServiceProvider.php
+++ Modules/Product/Providers/ProductServiceProvider.php
@@ -27,6 +27,7 @@ class ProductServiceProvider extends ServiceProvider
         $this->registerTranslations();
         $this->registerConfig();
         $this->registerViews();
+        $this->registerFactories();
         $this->loadMigrationsFrom(module_path($this->moduleName, 'Database/Migrations'));
     }
 
@@ -86,6 +87,18 @@ class ProductServiceProvider extends ServiceProvider
             $this->loadTranslationsFrom($langPath, $this->moduleNameLower);
         } else {
             $this->loadTranslationsFrom(module_path($this->moduleName, 'Resources/lang'), $this->moduleNameLower);
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path($this->moduleName, 'Database/factories'));
         }
     }
 
